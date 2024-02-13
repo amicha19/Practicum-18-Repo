81b3f4f7912b9efc06b0ce0f7522dec4242578b7
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-13 08:43:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,22 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    
-    System.out.print("Enter your age: ");
-    int age = input.nextInt();
-    
-    System.out.print("Enter your first name: ");
-    String first_name = input.nextLine();
-    System.out.print("Enter your last name: ");
-    String last_name = input.nextLine();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    System.out.println("Age = " + age);
-    System.out.println("Name = " + first_name + " " +
-                       last_name);
-    
-  }
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        // Consume the newline character
+        input.nextLine();
+
+        System.out.print("Enter your first name: ");
+        String first_name = input.nextLine();
+        System.out.print("Enter your last name: ");
+        String last_name = input.nextLine();
+
+        System.out.println("Age = " + age);
+        System.out.println("Name = " + first_name + " " +
+                last_name);
+    }
 }
